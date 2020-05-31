5610ab586ddc0b584b1492f0b9afd087c3b190a7
santamaria <pauliix.santamaria@gmail.com>
2020-05-31 16:37:15 +0100

    user list done

--- src/app/session/session.module.ts
+++ src/app/session/session.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { IonicModule } from "@ionic/angular";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { SessionPageRoutingModule } from "./session-routing.module";
 import { SessionCardComponent } from "../sessions-list/session-card/session-card.component";
@@ -10,7 +11,12 @@ import { SessionPage } from "./session.page";
 import { AbsentPipe } from "../pipes/absent.pipe";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SessionPageRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    SessionPageRoutingModule,
+  ],
   declarations: [SessionPage, SessionCardComponent, AbsentPipe],
 })
 export class SessionPageModule {}
